330831532738fa372c24b8a54ed9593182966a20
JeromeDias <55798382+JeromeDias@users.noreply.github.com>
2020-02-13 13:49:33 +0100

    mod

---
+++ build/precache-manifest.9388164003774f684f82769e6a93626e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d47c820c2419c8a233c32cb87a9c7910",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67efbfe42aad3e254bdf",
+    "url": "/static/css/main.04e1594d.chunk.css"
+  },
+  {
+    "revision": "5c937dfd45f15b406fd5",
+    "url": "/static/js/2.9a96a7bc.chunk.js"
+  },
+  {
+    "revision": "67efbfe42aad3e254bdf",
+    "url": "/static/js/main.eda62360.chunk.js"
+  },
+  {
+    "revision": "0ccfaae032ceb2a13aa8",
+    "url": "/static/js/runtime-main.5f5fd4e6.js"
+  }
+]);
